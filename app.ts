4ffc5f984727f5aa707178affb87761378d98780
Waqas Haneef <waqas.haneef1@hotmail.com>
2024-09-08 08:26:23 +0500

    Initial Check In

---
+++ app.ts
@@ -0,0 +1,3 @@
+let greeting: string = "Hello Type Script";
+
+console.log(`This is my first TYPE Script : ${greeting}`);
---
+++ js/app.js
@@ -0,0 +1,3 @@
+"use strict";
+let greeting = "Hello Type Script";
+console.log(`THis is my first TYPE Script : ${greeting}`);
