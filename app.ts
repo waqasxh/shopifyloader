a47d3f44b5c883a8b9202a6c375dd75aba40c149
Waqas Haneef <waqas.haneef1@hotmail.com>
2024-10-06 13:53:31 +0500

    Add Product using ProductSet Mutation v3

--- app.ts
+++ app.ts
@@ -4,17 +4,26 @@ import {
   retrievProductById,
   unpublishProductById,
   addProductSet,
+  addProductSetEx,
 } from "./processors/shopify";
 
 let name: string = "Shopify Loader";
 logger.info(`Execution of  ${name} Started.`);
 
 //const records = processFile();
+
 //convertAndExportFile();
+
 //retrievProductById("gid://shopify/Product/9179310391638");
 // unpublishProductById(
 //   "gid://shopify/Product/9179310391638",
 //   "gid://shopify/Publication/248231526742",
 //   "2024-09-23T02:31:51Z"
 // );
-addProductSet();
+
+//addProductSet();
+
+const result = addProductSetEx().then((data) => {
+  const id = data.productSet.product.id;
+  const handle = data.productSet.product.handle;
+});
